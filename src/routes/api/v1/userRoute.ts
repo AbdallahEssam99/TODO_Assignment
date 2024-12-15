22137b359d098810e2cb8cc468146c2734ce99b8
AbdallahEssam99 <AbdallahEssam99>
2024-12-15 16:03:47 +0200

    userRouter

--- src/routes/api/v1/userRoute.ts
+++ src/routes/api/v1/userRoute.ts
@@ -1,13 +1,13 @@
-import express from 'express';
+import {Router} from 'express';
 import {createUser, getAllUsers, getUser,updateUser,deleteUser,userLogin} from '../../../services/userService';
 
+const router = Router();
 
-const userRouter = (app: express.Application) => {
-    app.get('/api/v1/users', getAllUsers);
-    app.get('/api/v1/users/:id', getUser);
-    app.post('/api/v1/users', createUser);
-    app.patch('/api/v1/users/:id', updateUser);
-    app.delete('/api/v1/users/:id', deleteUser);
-    app.post('/api/v1/users/login', userLogin);
-}
-export default userRouter;
+router.get('/', getAllUsers);
+router.get('/:id', getUser);
+router.post('/', createUser);
+router.patch('/:id', updateUser);
+router.delete('/:id', deleteUser);
+router.post('/login', userLogin);
+
+export default router;
