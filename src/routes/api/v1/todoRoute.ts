06ca8276760df9c96a6e504539825e5bb2217860
AbdallahEssam99 <AbdallahEssam99>
2024-12-15 16:03:14 +0200

    todoRouter

--- src/routes/api/v1/todoRoute.ts
+++ src/routes/api/v1/todoRoute.ts
@@ -1,15 +1,13 @@
-import express from 'express';
+import {Router} from 'express';
 import { getAllTodos,getTodo,createTodo,updateTodo,deleteTodo,getUserTodos} from '../../../services/todoSevice';
 
-const todoRouter = (app: express.Application) => {
+const router = Router();
 
-  app.get('/api/v1/todos', getAllTodos);
-  app.get('/api/v1/todos/:id', getTodo);
-  app.post('/api/v1/todos', createTodo);
-  app.patch('/api/v1/todos/:id', updateTodo);
-  app.delete('/api/v1/todos/:id', deleteTodo);
-  app.get('/api/v1/users/:userId/todos', getUserTodos);
+router.get('/', getAllTodos);
+router.get('/:id', getTodo);
+router.post('/', createTodo);
+router.patch('/:id', updateTodo);
+router.delete('/:id', deleteTodo);
+router.get('/user/:id', getUserTodos);
 
-}
-
-export default todoRouter;
+export default router;
